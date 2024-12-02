8055c2b03af8db4b6ee23b44c6c28a7d7c20abbb
Alex <alexzhang1030@foxmail.com>
2024-12-02 16:12:15 +0800

    feat: day01 part_2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 /// https://adventofcode.com/2024/day/1
 /// ```
 /// 3 4
@@ -23,9 +25,55 @@
 ///
 /// Answer: 2 + 1 + 0 + 1 + 2 + 5 = 11
 #[allow(dead_code)]
-fn day01() -> Result<i32, String> {
+fn day01_part1() -> Result<i32, String> {
   // Time complexity: O(n log n)
   // TODO: Optimize to O(n)
+  let (mut left, mut right) = from_str()?;
+
+  left.sort();
+  right.sort();
+
+  Ok(left.iter().zip(right.iter()).map(|(l, r)| (l - r).abs()).sum())
+}
+
+/// Part two
+/// ```
+/// 3 4
+/// 4 3
+/// 2 5
+/// 1 3
+/// 3 9
+/// 3 3
+/// ```
+///
+/// Rules:
+/// - Calculate similarity score
+/// - by adding up each number in the left list after multiplying it
+/// - by the number of times that number appears in the right list.
+/// - Answer is the sum of all the scores
+///
+/// Example above:
+/// - 3: 3 * 3(appears 3 times) = 9
+/// - 4: 4 * 1(appears 1 time) = 4
+/// - 2: 2 * 0(does't appears) = 0
+/// - 1: 1 * 0(does't appears) = 0
+/// - 3: 3 * 3(appears 3 times) = 9
+/// - 3: 3 * 3(appears 3 times) = 9
+///
+/// Answer: 9 + 4 + 0 + 0 + 9 + 9 = 31
+#[allow(dead_code)]
+fn day01_part2() -> Result<i32, String> {
+  let (left, right) = from_str()?;
+
+  let mut count_map: HashMap<i32, i32> = HashMap::new();
+  for num in right {
+    *count_map.entry(num).or_insert(0) += 1;
+  }
+
+  Ok(left.iter().filter_map(|&num| count_map.get(&num).map(|&count| count * num)).sum())
+}
+
+fn from_str() -> Result<(Vec<i32>, Vec<i32>), String> {
   let text = include_str!("./input.txt");
 
   let mut left = Vec::new();
@@ -39,19 +87,22 @@ fn day01() -> Result<i32, String> {
     right.push(r.parse::<i32>().map_err(|e| format!("Invalid number '{}': {}", r, e))?);
   }
 
-  left.sort();
-  right.sort();
-
-  Ok(left.iter().zip(right.iter()).map(|(l, r)| (l - r).abs()).sum())
+  Ok((left, right))
 }
 
 #[cfg(test)]
 mod test {
-  use super::day01;
+  use super::{day01_part1, day01_part2};
 
   #[test]
-  fn test_day01() {
-    let res = day01().unwrap_or(-1);
+  fn test_day01_part_1() {
+    let res = day01_part1().unwrap_or(-1);
     assert_eq!(res, 2176849);
+  }
+
+  #[test]
+  fn test_day02_part_2() {
+    let res = day01_part2().unwrap_or(-1);
+    assert_eq!(res, 23384288);
   }
 }
